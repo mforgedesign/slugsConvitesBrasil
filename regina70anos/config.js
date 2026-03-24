5446b690a3aa2e54ed97e38a06e29c1fda4dda50
mforgedesign <mforgedesign@gmail.com>
2026-03-24 08:04:12 -0300

    Deploy convite: regina70anos

---
+++ regina70anos/config.js
@@ -0,0 +1,67 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversário",
+    "nome": "Regina ",
+    "idade": 70,
+    "data": "2026-06-26",
+    "hora": "16:30",
+    "endereco": "Buffet Dream Garden Rua Borcelle, 312 - Brasilândia São Paulo - SP"
+  },
+  "convite": {
+    "paletaCores": "Azul degradê e branco",
+    "tema": "Flores naturais",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/regina70anos/music.mp3",
+    "tipoAbertura": "longa",
+    "particulasAbertura": true,
+    "slug": "regina70anos",
+    "seo": {
+      "pageTitle": "Convite 70 Anos - Regina",
+      "ogTitle": "Você está convidado para os 70 anos da Regina!",
+      "ogDescription": "Embarque nessa noite mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/regina70anos/cover.jpg",
+    "aberturaSlides": [
+      "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/regina70anos/slide1.mp4"
+    ],
+    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/regina70anos/folha.png",
+    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/regina70anos/folha.png",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/regina70anos/music.mp3",
+    "fotos": [],
+    "popupImagensPorBotao": {}
+  },
+  "botoes": [
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://maps.app.goo.gl/snj7sG3GK9DfT6rh6"
+    },
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "ConfirmarPresença",
+      "icone": "fa-solid fa-check",
+      "conteudo": "https://docs.google.com/forms/d/e/1FAIpQLScEg-q-tulnW38QXgLmdou-cwUsUDIyDI4IKoTaS_S82GxY3Q/viewform?usp=publish-editor"
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": true
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "buttonColor": "#2c569b",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
