f773eeeed5feec4527991a479c64d12050e5585e
mforgedesign <mforgedesign@gmail.com>
2026-03-26 10:30:55 -0300

    Deploy convite: savethedate-regina70anos

--- savethedate-regina70anos/config.js
+++ savethedate-regina70anos/config.js
@@ -2,7 +2,7 @@ window.config = {
   "productType": "savethedate",
   "convite": {
     "slug": "savethedate-regina70anos",
-    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/music_1774531003900.mp3",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/music_1774531851431.mp3",
     "seo": {
       "pageTitle": "Convite: 15 Anos - Regina",
       "ogTitle": "Você está convidado para os 70 anos da Regina!",
@@ -10,9 +10,9 @@ window.config = {
     }
   },
   "assets": {
-    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/folha_1774531003900.jpg",
-    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/music_1774531003900.mp3",
-    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/folha_1774531003900.jpg"
+    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/folha_1774531851431.jpg",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/music_1774531851431.mp3",
+    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/folha_1774531851431.jpg"
   },
   "config": {
     "brilhos": {
