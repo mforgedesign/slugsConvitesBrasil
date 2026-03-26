33c2ee60e6c4b5e45412acbdf4a0bb9b356a874f
mforgedesign <mforgedesign@gmail.com>
2026-03-26 20:16:03 -0300

    Deploy convite: savethedate-regina70anos

--- savethedate-regina70anos/config.js
+++ savethedate-regina70anos/config.js
@@ -2,23 +2,37 @@ window.config = {
   "productType": "savethedate",
   "convite": {
     "slug": "savethedate-regina70anos",
-    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/music_1774531851431.mp3",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/music_1774566957760.mp3",
     "seo": {
       "pageTitle": "Convite: 15 Anos - Regina",
       "ogTitle": "Você está convidado para os 70 anos da Regina!",
       "ogDescription": "Embarque nessa noite mágica. Clique para ver os detalhes."
     }
   },
+  "botoes": [
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "",
+      "icone": "fa-solid fa-star",
+      "conteudo": "https://docs.google.com/forms/d/e/1FAIpQLScEg-q-tulnW38QXgLmdou-cwUsUDIyDI4IKoTaS_S82GxY3Q/viewform?usp=publish-editor"
+    }
+  ],
   "assets": {
-    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/folha_1774531851431.jpg",
-    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/music_1774531851431.mp3",
-    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/folha_1774531851431.jpg"
+    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/folha_1774566957760.jpg",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/music_1774566957760.mp3",
+    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/folha_1774566957760.jpg"
   },
   "config": {
     "brilhos": {
       "esquerdo": true,
       "direito": true,
       "centro": true
+    },
+    "botoes": {
+      "cor": "#ffffff",
+      "tamanho": 1,
+      "preenchido": true
     }
   }
 };
