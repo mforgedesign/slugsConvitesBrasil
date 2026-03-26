ad1d9f9e5396192c24a15543dfeef2c581aa4bc2
mforgedesign <mforgedesign@gmail.com>
2026-03-26 10:16:47 -0300

    Deploy convite: savethedate-regina70anos

---
+++ savethedate-regina70anos/config.js
@@ -0,0 +1,24 @@
+window.config = {
+  "productType": "savethedate",
+  "convite": {
+    "slug": "savethedate-regina70anos",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/music_1774531003900.mp3",
+    "seo": {
+      "pageTitle": "Convite: 15 Anos - Regina",
+      "ogTitle": "Você está convidado para os 70 anos da Regina!",
+      "ogDescription": "Embarque nessa noite mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/folha_1774531003900.jpg",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/music_1774531003900.mp3",
+    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/savethedate-regina70anos/folha_1774531003900.jpg"
+  },
+  "config": {
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
