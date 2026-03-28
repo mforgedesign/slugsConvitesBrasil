26943243f8132e4895e6055f325d6ae213743c5e
mforgedesign <mforgedesign@gmail.com>
2026-03-28 08:00:27 -0300

    Deploy convite: ana-julia15anos

---
+++ ana-julia15anos/config.js
@@ -0,0 +1,76 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversário",
+    "nome": "Ana Julia",
+    "idade": 15,
+    "data": "2026-12-12",
+    "hora": "21:00",
+    "endereco": "JARDIM SECRETO - PARQUE SÃO JORGE  - R. das Araribás, 25 - Bairro dos Casa, São Bernardo do Campo"
+  },
+  "convite": {
+    "paletaCores": "Azul Marinho, Rosa Bebê e Prata",
+    "tema": "15 Anos",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ana-julia15anos/music_1774695621785.mp3",
+    "tipoAbertura": "longa",
+    "particulasAbertura": true,
+    "slug": "ana-julia15anos",
+    "seo": {
+      "pageTitle": "Convite: 15 Anos - Ana Julia",
+      "ogTitle": "Você está convidado para os 15 anos da Ana Julia!",
+      "ogDescription": "Embarque nessa noite mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ana-julia15anos/cover_1774695621785.jpg",
+    "aberturaSlides": [
+      "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ana-julia15anos/slide1_1774695621785.mp4"
+    ],
+    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ana-julia15anos/folha_1774695621785.jpg",
+    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ana-julia15anos/folha_1774695621785.jpg",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ana-julia15anos/music_1774695621785.mp3",
+    "fotos": [],
+    "popupImagensPorBotao": {
+      "1": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ana-julia15anos/43a3edba-300c-492b-8396-a17cfe159de7_1774695621785.jpg"
+    }
+  },
+  "botoes": [
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://maps.app.goo.gl/Zzk2EMHetN6bcMQQ7"
+    },
+    {
+      "tipoAcao": "PopupImagem",
+      "tipoVisual": "css",
+      "titulo": "Dicas dePresente",
+      "icone": "fa-solid fa-gift",
+      "conteudo": "assets/43a3edba-300c-492b-8396-a17cfe159de7.jpg"
+    },
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "ConfirmarPresença",
+      "icone": "fa-solid fa-check",
+      "conteudo": "https://docs.google.com/forms/d/e/1FAIpQLSc9LjVUP6zxbA7AldzHQ7rqfrJQD17I347ilGFDbaPdfxRFjQ/viewform?usp=publish-editor"
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": true
+  },
+  "config": {
+    "exibirMarcaDagua": true,
+    "buttonColor": "#0c2b71",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
