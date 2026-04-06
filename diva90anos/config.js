45437b8516238da5e25439651896773661ab60ca
mforgedesign <mforgedesign@gmail.com>
2026-04-06 20:13:25 -0300

    Deploy convite: diva90anos

---
+++ diva90anos/config.js
@@ -0,0 +1,72 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversario",
+    "nome": "Diva Vidal",
+    "idade": 90,
+    "data": "2026-06-06",
+    "hora": "13:00",
+    "endereco": "Rua Flavita Bretas, 226 salão de festas - Luxemburgo"
+  },
+  "convite": {
+    "paletaCores": "verde, lilás, dourado",
+    "tema": "",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/diva90anos/Musica_1775517196748.mp3",
+    "tipoAbertura": "curta",
+    "particulasAbertura": true,
+    "slug": "diva90anos",
+    "seo": {
+      "pageTitle": "Convite: Aniversario 90 anos - Diva Vidal",
+      "ogTitle": "Você está convidado para o evento de Diva Vidal!",
+      "ogDescription": "Embarque nessa experiência mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/diva90anos/capa_1775517196748.jpg",
+    "aberturaSlides": [
+      "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/diva90anos/slide-1_1775517196748.mp4"
+    ],
+    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/diva90anos/folha-preenchida_1775517196748.jpg",
+    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/diva90anos/folha-preenchida_1775517196748.jpg",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/diva90anos/Musica_1775517196748.mp3",
+    "fotos": []
+  },
+  "botoes": [
+    {
+      "tipoAcao": "RSVP",
+      "tipoVisual": "css",
+      "titulo": "Confirmar\nPresenca",
+      "icone": "fa-solid fa-check",
+      "whatsapp": "5511940820140",
+      "rsvpConfig": {
+        "exibirNome": true,
+        "exibirWhatsapp": false,
+        "exibirAcompanhantes": false
+      }
+    },
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://www.google.com/maps/search/?api=1&query=Rua%20Flavita%20Bretas%2C%20226%20sal%C3%A3o%20de%20festas%20-%20Luxemburgo"
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": false
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "cronometro": false,
+    "buttonColor": "#6a2c7a",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
