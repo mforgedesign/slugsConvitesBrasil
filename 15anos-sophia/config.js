a513f37d37c96aa913bf24b9153b31401a5ada3f
mforgedesign <mforgedesign@gmail.com>
2026-04-28 17:03:19 -0300

    Deploy convite: 15anos-sophia

---
+++ 15anos-sophia/config.js
@@ -0,0 +1,63 @@
+window.config = {
+  "evento": {
+    "tipo": "Debutante",
+    "nome": "Sophia",
+    "idade": 15,
+    "data": "2026-08-28",
+    "hora": "18:40",
+    "endereco": "Avenida Imperador 3660"
+  },
+  "convite": {
+    "paletaCores": "Pink, Rosé e Prata",
+    "tema": "Floral Borboletas",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/15anos-sophia/Musica_1777406592297.mp3",
+    "tipoAbertura": "curta",
+    "particulasAbertura": true,
+    "slug": "15anos-sophia",
+    "seo": {
+      "pageTitle": "Convite: Debutante 15 anos - Sophia",
+      "ogTitle": "Você está convidado para o evento de Sophia!",
+      "ogDescription": "Embarque nessa experiência mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "assets/Capa.jpg",
+    "aberturaSlides": [],
+    "folhaVazia": "assets/Folha Preenchida.jpg",
+    "folhaPreenchida": "assets/Folha Preenchida.jpg",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/15anos-sophia/Musica_1777406592297.mp3",
+    "fotos": []
+  },
+  "botoes": [
+    {
+      "tipoAcao": "RSVP",
+      "tipoVisual": "css",
+      "titulo": "Confirmar\nPresença",
+      "icone": "fa-solid fa-check",
+      "whatsapp": "5511990244780",
+      "rsvpConfig": {
+        "exibirNome": true,
+        "exibirWhatsapp": false,
+        "exibirAcompanhantes": false
+      }
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": false
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "cronometro": true,
+    "buttonColor": "#c71585",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
