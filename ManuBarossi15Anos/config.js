4087209afe0931417cffba9cdef15964a4c29c9a
mforgedesign <mforgedesign@gmail.com>
2026-05-04 14:56:54 -0300

    Deploy convite: ManuBarossi15Anos

---
+++ ManuBarossi15Anos/config.js
@@ -0,0 +1,77 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversário",
+    "nome": "Manu Barossi",
+    "idade": 15,
+    "data": "2026-08-04",
+    "hora": "20:00",
+    "endereco": ""
+  },
+  "convite": {
+    "paletaCores": "",
+    "tema": "",
+    "musica": "",
+    "tipoAbertura": "longa",
+    "particulasAbertura": true,
+    "slug": "ManuBarossi15Anos",
+    "seo": {
+      "pageTitle": "Manu Barossi | 15 Anos",
+      "ogTitle": "Você está convidado para os 15 anos da Manu!",
+      "ogDescription": "Embarque nessa noite mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ManuBarossi15Anos/cover_1777917405287.jpg",
+    "aberturaSlides": [
+      "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ManuBarossi15Anos/slide1_1777917405287.mp4",
+      "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ManuBarossi15Anos/slide2_1777917405287.mp4",
+      "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ManuBarossi15Anos/slide3_1777917405287.jpg",
+      "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ManuBarossi15Anos/slide4_1777917405287.mp4",
+      "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ManuBarossi15Anos/slide5_1777917405287.jpg"
+    ],
+    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ManuBarossi15Anos/folha_1777917405287.jpg",
+    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/ManuBarossi15Anos/folha_1777917405287.jpg",
+    "musica": "",
+    "fotos": [],
+    "popupImagensPorBotao": {}
+  },
+  "botoes": [
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://maps.app.goo.gl/SJ6ehmnUztm1k7288"
+    },
+    {
+      "tipoAcao": "RSVP",
+      "tipoVisual": "css",
+      "titulo": "Confirmar Presença",
+      "icone": "fa-solid fa-check",
+      "whatsapp": "5511947219432",
+      "rsvpConfig": {
+        "exibirNome": true,
+        "exibirWhatsapp": false,
+        "exibirAcompanhantes": false
+      }
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": false
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "cronometro": false,
+    "buttonColor": "#0c2b71",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
