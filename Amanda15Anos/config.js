69e1313e27c528b377aad4a430b23cc6cfd0ab36
mforgedesign <mforgedesign@gmail.com>
2026-05-06 11:12:00 -0300

    Deploy convite: Amanda15Anos

--- Amanda15Anos/config.js
+++ Amanda15Anos/config.js
@@ -10,7 +10,7 @@ window.config = {
   "convite": {
     "paletaCores": "",
     "tema": "",
-    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Amanda15Anos/music_1778006468324.mp3",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Amanda15Anos/music_1778076712720.mp3",
     "tipoAbertura": "longa",
     "particulasAbertura": true,
     "slug": "Amanda15Anos",
@@ -21,13 +21,13 @@ window.config = {
     }
   },
   "assets": {
-    "capa": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Amanda15Anos/cover_1778006468324.jpg",
+    "capa": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Amanda15Anos/cover_1778076712720.jpg",
     "aberturaSlides": [
-      "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Amanda15Anos/slide1_1778006468324.mp4"
+      "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Amanda15Anos/slide1_1778076712720.mp4"
     ],
-    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Amanda15Anos/folha_1778006468324.jpg",
-    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Amanda15Anos/folha_1778006468324.jpg",
-    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Amanda15Anos/music_1778006468324.mp3",
+    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Amanda15Anos/folha_1778076712720.jpg",
+    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Amanda15Anos/folha_1778076712720.jpg",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Amanda15Anos/music_1778076712720.mp3",
     "fotos": [],
     "popupImagensPorBotao": {}
   },
