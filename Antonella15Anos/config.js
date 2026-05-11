7d6e52f86ee10d52de64556877e1d83af28489ab
mforgedesign <mforgedesign@gmail.com>
2026-05-11 08:38:32 -0300

    Deploy convite: Antonella15Anos

--- Antonella15Anos/config.js
+++ Antonella15Anos/config.js
@@ -10,7 +10,7 @@ window.config = {
   "convite": {
     "paletaCores": "",
     "tema": "",
-    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Antonella15Anos/music_1778499399729.mp3",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Antonella15Anos/music_1778499506287.mp3",
     "tipoAbertura": "longa",
     "particulasAbertura": true,
     "slug": "Antonella15Anos",
@@ -21,13 +21,13 @@ window.config = {
     }
   },
   "assets": {
-    "capa": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Antonella15Anos/cover_1778499399729.mp4",
+    "capa": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Antonella15Anos/cover_1778499506287.jpg",
     "aberturaSlides": [
-      "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Antonella15Anos/slide1_1778499399729.mp4"
+      "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Antonella15Anos/slide1_1778499506287.mp4"
     ],
-    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Antonella15Anos/folha_1778499399729.jpg",
-    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Antonella15Anos/folha_1778499399729.jpg",
-    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Antonella15Anos/music_1778499399729.mp3",
+    "folhaVazia": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Antonella15Anos/folha_1778499506287.jpg",
+    "folhaPreenchida": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Antonella15Anos/folha_1778499506287.jpg",
+    "musica": "https://qhrjyhnbcqllbsfuzlmj.supabase.co/storage/v1/object/public/convite-assets/Antonella15Anos/music_1778499506287.mp3",
     "fotos": [],
     "popupImagensPorBotao": {}
   },
