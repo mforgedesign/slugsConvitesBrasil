898aaef2c88a2be44d6a528d2d0a0209afa5bdfe
mforgedesign <mforgedesign@gmail.com>
2026-05-21 10:01:47 -0300

    Deploy convite: aniversariante-festa-surpresa

---
+++ aniversariante-festa-surpresa/config.js
@@ -0,0 +1,63 @@
+window.config = {
+  "evento": {
+    "tipo": "Aniversário",
+    "nome": "aniversariante",
+    "idade": null,
+    "data": "2026-05-15",
+    "hora": "19:00",
+    "endereco": "Espaço Villa Verde"
+  },
+  "convite": {
+    "paletaCores": "",
+    "tema": "Festa de aniversário surpresa",
+    "musica": "assets/musica_1779368438660.mp3",
+    "tipoAbertura": "curta",
+    "particulasAbertura": true,
+    "slug": "aniversariante-festa-surpresa",
+    "seo": {
+      "pageTitle": "Convite: Aniversário - aniversariante",
+      "ogTitle": "Você está convidado para os  anos de aniversariante!",
+      "ogDescription": "Embarque nessa experiência mágica. Clique para ver os detalhes."
+    }
+  },
+  "assets": {
+    "capa": "assets/capa_1779368438660.jpg",
+    "aberturaSlides": [
+      "assets/Slide3_1779368438660.mp4",
+      "assets/slide1_1779368438660.mp4",
+      "assets/slide2_1779368438660.mp4",
+      "assets/slide4_1779368438660.mp4",
+      "assets/slide5_1779368438660.mp4"
+    ],
+    "folhaVazia": "assets/Folha Preenchida_1779368438660.jpg",
+    "folhaPreenchida": "assets/Folha Preenchida_1779368438660.jpg",
+    "musica": "assets/musica_1779368438660.mp3",
+    "fotos": []
+  },
+  "botoes": [
+    {
+      "tipoAcao": "Link",
+      "tipoVisual": "css",
+      "titulo": "Como Chegar",
+      "icone": "fa-solid fa-location-dot",
+      "conteudo": "https://www.google.com/maps/search/?api=1&query=Espa%C3%A7o%20Villa%20Verde"
+    }
+  ],
+  "upsell": {
+    "galeriaFotos": false,
+    "saveTheDate": false,
+    "lembrete": false
+  },
+  "config": {
+    "exibirMarcaDagua": false,
+    "buttonColor": "#0c2b71",
+    "buttonSize": 1,
+    "isButtonFilled": true,
+    "shadowStyle": "linear-gradient(to top, rgba(0,0,0,0.85) 0%, rgba(0,0,0,0) 100%)",
+    "brilhos": {
+      "esquerdo": true,
+      "direito": true,
+      "centro": true
+    }
+  }
+};
