2cfb791d68384657807d90249e5a28cce88cd952
Builder v6.1 <builder@mforge.com.br>
2026-06-18 14:35:06 -0300

    Deploy convite: Voce-E-Meu-Convidado-Especial

--- Voce-E-Meu-Convidado-Especial/config.js
+++ Voce-E-Meu-Convidado-Especial/config.js
@@ -10,7 +10,7 @@ window.config = {
   "convite": {
     "paletaCores": "",
     "tema": "",
-    "musica": "assets/music_1779385611076.mp3",
+    "musica": "assets/music_1781804105930.mp3",
     "tipoAbertura": "longa",
     "particulasAbertura": true,
     "slug": "Voce-E-Meu-Convidado-Especial",
@@ -21,13 +21,13 @@ window.config = {
     }
   },
   "assets": {
-    "capa": "assets/cover_1779385611076.jpg",
+    "capa": "assets/cover_1781804105930.jpg",
     "aberturaSlides": [
-      "assets/slide1_1779385611076.mp4"
+      "assets/slide1_1781804105930.mp4"
     ],
-    "folhaVazia": "assets/folha_1779385611076.jpg",
-    "folhaPreenchida": "assets/folha_1779385611076.jpg",
-    "musica": "assets/music_1779385611076.mp3",
+    "folhaVazia": "assets/folha_1781804105930.jpg",
+    "folhaPreenchida": "assets/folha_1781804105930.jpg",
+    "musica": "assets/music_1781804105930.mp3",
     "fotos": [],
     "popupImagensPorBotao": {}
   },
@@ -50,6 +50,13 @@ window.config = {
         "exibirWhatsapp": false,
         "exibirAcompanhantes": false
       }
+    },
+    {
+      "tipoAcao": "PopupHtml",
+      "tipoVisual": "css",
+      "titulo": "Sugestão de Presente",
+      "icone": "fa-solid fa-book-open",
+      "conteudo": "Anel tamanho 16\nCalçado tamanho 37\nRoupas: calça 36, blusa P\nCores favoritas Preta e Branca\nVale presente: O Boticário, Sephora, PopMe, Renner, Livrarias Curitiba."
     }
   ],
   "upsell": {
